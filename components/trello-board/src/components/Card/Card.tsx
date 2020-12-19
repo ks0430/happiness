0e02ac935d6743c208627ac640d5f3f75e1f80aa
evelyn <ore.c0430@gmail.com>
2020-12-19 17:14:49 +1100

    custom card test

--- components/trello-board/src/components/Card/Card.tsx
+++ components/trello-board/src/components/Card/Card.tsx
@@ -1,31 +1,64 @@
-import React, { Component } from "react";
-import { Button, Box, Text } from "@chakra-ui/react";
+import React, { Component } from 'react'
+import {
+    Button,
+    Box,
+    Text,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    useDisclosure,
+} from '@chakra-ui/react'
 
 const Card = (props) => {
-  const {
-    showDeleteButton,
-    style,
-    tagStyle,
-    onClick,
-    onDelete,
-    className,
-    id,
-    title,
-    label,
-    description,
-    tags,
-    cardDraggable,
-  } = props;
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const {
+        showDeleteButton,
+        style,
+        tagStyle,
+        onClick,
+        onDelete,
+        className,
+        id,
+        title,
+        label,
+        description,
+        tags,
+        cardDraggable,
+    } = props
 
-  //   console.log(props);
-  return (
-    <Box bg="white" w="100%" p={4} color="white">
-      <Text fontSize="xl" color="gray.500">
-        Test
-      </Text>
-      <Button colorScheme="blue">Button</Button>
-    </Box>
-  );
-};
+    //   console.log(props);
+    return (
+        <Box onClick={onOpen} bg="white" w="100%" p={4} color="white">
+            <Text fontSize="xl" color="gray.500">
+                {title}
+            </Text>
+            <Text fontSize="md" color="gray.500">
+                {description}
+            </Text>
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Modal Title</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <Text fontSize="xl" color="gray.500">
+                            Test
+                        </Text>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button colorScheme="blue" mr={3} onClick={onClose}>
+                            Close
+                        </Button>
+                        <Button variant="ghost">Secondary Action</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </Box>
+    )
+}
 
-export default Card;
+export default Card
